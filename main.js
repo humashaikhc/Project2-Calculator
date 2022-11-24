c296bab6d3f5abfe31a525893a9ac1590e5bb0d9
humashaikhc <fatma_shaikh21@yahoo.com>
2022-11-24 10:40:13 +0000

    Final commit

--- main.js
+++ main.js
@@ -6,17 +6,15 @@ const numbers = document.querySelectorAll(".number");
 const operation = document.querySelectorAll(".operation");
 const equal = document.querySelector(".equal");
 const clearAll = document.querySelector(".all-clear");
-const clearLast = document.querySelector(".last-clear");
 
 //Empty dis1Num, dis2Num and lastOperation so that input and operations can be done.
-
 let dis1Num = "";
 let dis2Num = "";
 let result = null;
 let lastOperation = "";
 let haveDot = false;
 
-//Adding event listener to the numbers 
+//Adding event listener to the numbers
 //Accepting only one decimal instead of multiple using if else if statements.
 numbers.forEach((number) => {
   number.addEventListener("click", (e) => {
@@ -26,7 +24,7 @@ numbers.forEach((number) => {
       return;
     }
 
-//Displaying input at display2. Concatening more than 1 digit number.
+    //Displaying input at display2. Concatening more than 1 digit number.
     dis2Num += e.target.innerText;
     display2.innerText = dis2Num;
   });
@@ -34,70 +32,61 @@ numbers.forEach((number) => {
 
 //Creating a math function for operations on buttons.
 function mathOperation() {
-    if (lastOperation === "x") {
-      result = parseFloat(result) * parseFloat(dis2Num);
-    } 
-    else if (lastOperation === "+") {
-      result = parseFloat(result) + parseFloat(dis2Num);
-    } 
-    else if (lastOperation === "-") {
-      result = parseFloat(result) - parseFloat(dis2Num);
-    } 
-    else if (lastOperation === "/") {
-      result = parseFloat(result) / parseFloat(dis2Num);
-    } 
-    else if (lastOperation === "%") {
-      result = parseFloat(result) % parseFloat(dis2Num);
-    }
+  if (lastOperation === "x") {
+    result = parseFloat(result) * parseFloat(dis2Num);
+  } else if (lastOperation === "+") {
+    result = parseFloat(result) + parseFloat(dis2Num);
+  } else if (lastOperation === "-") {
+    result = parseFloat(result) - parseFloat(dis2Num);
+  } else if (lastOperation === "/") {
+    result = parseFloat(result) / parseFloat(dis2Num);
   }
+}
 
 //A function to rearrange the display after pressing inputs..
-  function clearVar(name = "") {
-    dis1Num += dis2Num + " " + name + " ";
-    display1.innerText = dis1Num;
-    display2.innerText = "";
-    dis2Num = "";
-    TempResult.innerText = result;
-  }
+function clearVar(name = "") {
+  dis1Num += dis2Num + " " + name + " ";
+  display1.innerText = dis1Num;
+  display2.innerText = "";
+  dis2Num = "";
+}
 
 //Adding event listener for each operations.Calling math function to do operations.
 operation.forEach((operation) => {
-    operation.addEventListener("click", (e) => {
-        if (!dis2Num) 
-            return;
-      haveDot = false;
-      const operationName = e.target.innerText;
-      if (dis1Num && dis2Num && lastOperation) {
-        mathOperation();
-      } 
-      else {
-        result = parseFloat(dis2Num);
-      }
-      clearVar(operationName);
-      lastOperation = operationName;
-      console.log(result);
-    });
-  });
-
-
-  //Checking the inputs and displaying the output on display2.
-  equal.addEventListener("click", () => {
-    if (!dis2Num || !dis1Num) return;
+  operation.addEventListener("click", (e) => {
+    if (!dis2Num) return;
     haveDot = false;
-    mathOperation();
-    clearVar();
-    display2.innerText = result;
-    TempResult.innerText = "";
-    dis2Num = result;
-    dis1Num = "";
+    //Operation parameters will be saved in operationName(eg, +, -, etc)
+    const operationName = e.target.innerText;
+    if (dis1Num && dis2Num && lastOperation) {
+      mathOperation();
+    } else {
+      result = parseFloat(dis2Num);
+    }
+    clearVar(operationName);
+    lastOperation = operationName;
+    console.log(result);
   });
+});
 
-  //Clearing the display
-  clearAll.addEventListener("click", () => {
-    dis1Num = "";
-    dis2Num = "";
-    result = "";
-    display1.innerText = "0";
-    display2.innerText = "0";
-    TempResult.innerText = "0";
-  });
+//Checking the inputs and displaying the output on display2.
+//Adding event listener to equal button.
+equal.addEventListener("click", () => {
+  if (!dis2Num || !dis1Num) return;
+  haveDot = false;
+  mathOperation();
+  clearVar();
+  display2.innerText = result;
+  dis2Num = result;
+  dis1Num = "";
+});
+
+//Clearing the display
+clearAll.addEventListener("click", () => {
+  dis1Num = "";
+  dis2Num = "";
+  result = "";
+  display1.innerText = "0";
+  display2.innerText = "0";
+  TempResult.innerText = "0";
+});
